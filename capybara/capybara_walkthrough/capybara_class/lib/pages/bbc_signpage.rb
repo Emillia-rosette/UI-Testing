4a0610ac971aa27c32b52a7a35069e4328e74db9
Tech-A66 <tech-a66@MacBook-Air-10.local>
2018-04-19 14:58:44 +0100

    empty signin test

--- capybara/capybara_walkthrough/capybara_class/lib/pages/bbc_signpage.rb
+++ capybara/capybara_walkthrough/capybara_class/lib/pages/bbc_signpage.rb
@@ -1,24 +1,34 @@
-require 'capybara/dsl'
+require 'capybara'
 
 class Signinpage
-	include Capybara::DSL
-# signin page URL
-SIGNIN_PAGE_URL = 'https://account.bbc.com/signin'
-USERNAME_INPUT = 'user-identifier-input'
-PASSWORD_INPUT = 'password-input'
-BUTTON_CLICK = 'submit-button'
+include Capybara::DSL
 
- def fill_in_username(name)
- 	fill_in(USERNAME_INPUT, :with => name)
- end
+	USERNAME_INPUT = 'user-identifier-input'
+	PASSWORD_INPUT = 'password-input'
+	BUTTON_CLICK = 'submit-button'
+	ERROR_MESSAGE = 'form-message-username'
+	GENERAL_MESSAGE_ERROR = 'form-message-general'
+	PASSWORD_ERROR_MESSAGE = 'form-message-password'
 
- def fill_in_password(password)
- 	fill_in(PASSWORD_INPUT , :with => password)
- end
- def click_sign_in_button
- 	find(BUTTON_CLICK).click_button
- end
- # def incorrect_password_text
- 	
- # end
+	def fill_in_username(username)
+	 fill_in(USERNAME_INPUT, :with => username)
+	end
+	def fill_in_password(password)
+	 fill_in(PASSWORD_INPUT , :with => password)
+	end
+
+	def click_sign_in_button
+	 click_button(BUTTON_CLICK)
+	end
+
+	def incorrect_password_text
+		find(:id, ERROR_MESSAGE).text
+	end
+
+	def check_error_messgae
+		find(:id, GENERAL_MESSAGE_ERROR).text
+	end
+	def check_passowrd_message
+		find(:id, PASSWORD_ERROR_MESSAGE).text
+	end
 end
--- capybara/capybara_walkthrough/capybara_class/spec/bbc_login_error_spec.rb
+++ capybara/capybara_walkthrough/capybara_class/spec/bbc_login_error_spec.rb
@@ -1,6 +1,11 @@
 require 'spec_helper'
 
 describe 'Incorrect user details produces valid error'do 
+# before(:all)do
+#     @bbc_site = BbcSite.new
+#     @bbc_homepage = @bbc_site.bbc_homepage
+#     @bbc_signin = @bbc_site.bbc_signin
+# end
 context'It should respond with the correct error when incorrect details are input'do
 
 	it'should produce an error when inputting an incorrect password to a avalid account'do
@@ -10,9 +15,14 @@ context'It should respond with the correct error when incorrect details are inpu
     	@bbc_site.bbc_signin.fill_in_username('dszfxdgdfgdf')
     	@bbc_site.bbc_signin.fill_in_password('safsezddrfcdxc5')
     	@bbc_site.bbc_signin.click_sign_in_button
-    	expect(@bbc_site.bbc_sign_in_page.incorrect_password_text).to eq 'Sorry, we can not find an account with that username.
-    	If you are over 13, try your email address instead or get help here.' 
-    	sleep 20
+    	expect(@bbc_site.bbc_signin.incorrect_password_text).to eq "Sorry, we can’t find an account with that username. If you're over 13, try your email address instead or get help here."
+        sleep 30
     end 
-end	
+end
+context'should respond with the correct error when no details are entered'do
+    it 'should poduce three errors when no username or password is entered'do
+        @bbc_site = Bbc_site.new 
+        expect(@bbc_site.bbc_signin.incorrect_password_text).to eq "Sorry, we can’t find an account with that username. If you're over 13, try your email address instead or get help here."
+    end
+ end	
 end
